fc078e589264a7badc7a0d14f9a4f2c60f4875be
Arin7712 <aringawande7712@gmail.com>
2024-11-21 16:04:04 +0530

    update

---
+++ components/ui/demos/scroll-based-velocity-demo.tsx
@@ -0,0 +1,11 @@
+import { VelocityScroll } from "../scroll-based-velocity";
+
+export function ScrollBasedVelocityDemo() {
+  return (
+    <VelocityScroll
+      text="Mo Marketing Agency"
+      default_velocity={5}
+      className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-blue-400 drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+    />
+  );
+}
